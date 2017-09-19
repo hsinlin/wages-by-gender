f61b46e5402172aed4d30e124f918a6f9f588c30
Hsin Lin <hsin119@gmail.com>
2017-09-19 16:53:28 -0700

    configure component to use http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  data: any = null;
+
+  constructor(private _http: Http) {
+    this.getWagesByGender();
+  }
+
+  private getWagesByGender() {
+    return this._http.get('https://data.seattle.gov/api/views/cf52-s8er/rows.json?api_key=SCC1c0Cove7ypmBeuf3dTX2WZOk6qEfCAki6MoNi')
+                .map((res: Response) => res.json())
+                .subscribe(data => {
+                  this.data = data;
+                  console.log(this.data);
+                });
+  }
 }
