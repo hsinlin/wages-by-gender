baf0ef5481e728825d4af90ad2878fa6f37e6437
Hsin Lin <hsin119@gmail.com>
2017-09-19 18:28:46 -0700

    implement table sortable by column

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,12 +14,36 @@ export class AppComponent {
     this.getWagesByGender();
   }
 
+  public sortBy(column, order = 'asc') {
+    if(!['title', 'femaleWages', 'maleWages'].includes(column)) return;
+    console.info(order === 'desc')
+
+    this.data.sort((a, b) => {
+      let itemA, itemB;
+      if(column === 'title') {
+        if(order === 'desc') return a[8] < b[8];
+        else return a[8] > b[8];
+      } else if(column === 'femaleWages') {
+        itemA = a[9];
+        itemB = b[9];
+      } else if (column === 'maleWages') {
+        itemA = a[12];
+        itemB = b[12];
+      }
+
+      if(order === 'desc') {
+        return itemB - itemA;
+      } else {
+        return itemA - itemB;
+      }
+    });
+  }
+
   private getWagesByGender() {
     return this._http.get('https://data.seattle.gov/api/views/cf52-s8er/rows.json?api_key=SCC1c0Cove7ypmBeuf3dTX2WZOk6qEfCAki6MoNi')
                 .map((res: Response) => res.json())
                 .subscribe(data => {
-                  this.data = data;
-                  console.log(this.data);
+                  this.data = data.data;
                 });
   }
 }
