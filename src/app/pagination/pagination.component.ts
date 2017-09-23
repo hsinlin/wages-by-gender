32763e53ff89710251d244d1c87a89c284865d18
Hsin Lin <hsin119@gmail.com>
2017-09-22 20:28:40 -0700

    add inputs and outputs in pagination component

--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.css']
 })
-export class PaginationComponent implements OnInit {
+export class PaginationComponent {
+
+  @Input() page: number;
+  @Input() count: number;
+  @Input() perPage: number;
+  @Input() pagesToShow: number;
+  @Input() loading: boolean;
+
+  @Output() goPrev = new EventEmitter<boolean>();
+  @Output() goNext = new EventEmitter<boolean>();
+  @Output() goPage = new EventEmitter<number>();
 
   constructor() { }
 
