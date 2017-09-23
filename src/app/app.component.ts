99e2851776604ad5a4e23d3f6c2e248947a66b12
Hsin Lin <hsin119@gmail.com>
2017-09-23 12:10:37 -0700

    implement pagination functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,19 @@ import * as _ from "lodash";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  data: any = null;
+  data = null;
+  fullData = null;
+
+  loading = false;
+  total = 0;
+  page = 1;
+  limit = 25;
 
   constructor(private _http: Http) {
     this.getWagesByGender();
   }
 
-  public sortBy(column, order = 'asc') {
+  sortBy(column, order = 'asc') {
     if(!['title', 'femaleWages', 'maleWages'].includes(column)) return;
 
     if(column === 'title') {
@@ -36,11 +42,35 @@ export class AppComponent {
     }
   }
 
+  goToPage(n: number): void {
+    this.page = n;
+    this.getWagesData();
+  }
+
+  onNext(): void {
+    if(this.page >= this.total / this.limit) return;
+    this.page++;
+    this.getWagesData();
+  }
+
+  onPrev(): void {
+    if(this.page <= 1) return;
+    this.page--;
+    this.getWagesData();
+  }
+
+  getWagesData() {
+    let s = (this.page - 1) * this.limit;
+    this.data = this.fullData.slice(s, s+this.limit-1);
+  }
+
   private getWagesByGender() {
     return this._http.get('https://data.seattle.gov/api/views/cf52-s8er/rows.json?api_key=SCC1c0Cove7ypmBeuf3dTX2WZOk6qEfCAki6MoNi')
                 .map((res: Response) => res.json())
                 .subscribe(data => {
-                  this.data = data.data;
+                  this.fullData = data.data;
+                  this.total = this.fullData.length || 0;
+                  this.getWagesData();
                 });
   }
 }
